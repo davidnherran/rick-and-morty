bbb5ead3fa564aabd28006fff2b3c414d569186b
davidnherran <davidnherran@gmail.com>
2020-06-18 00:10:12 -0500

    retorno de objetos desde la API ok

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,32 @@
 import API from './api.js'
 const api = new API()
+const $characterContainer = document.querySelector('#character-container')
 
-console.log(api.getCharacter(3))
+class Character {
+    constructor({ name, image }){
+        this.name=name
+        this.image=image
+        this.render()
+    }
+    build(){ 
+        return `
+        <article class="character">
+            <div class="character-grid">
+               <h2>${this.name}</h2>
+               <img src=${this.image} alt="">
+            </div>
+        </article>
+        `
+    }
+    render(){
+        $characterContainer.innerHTML = this.build()
+    }
+}
+
+
+async function initApp(initCharacterId){
+    const characterData = await api.getCharacter(initCharacterId)
+        const rick=new Character(characterData)
+}
+
+initApp(2)
